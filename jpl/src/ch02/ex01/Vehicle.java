286bc7bc03c73399a17c3848e8da32fe09b1bbd6
sakemi <satoshi.sakemi@gmail.com>
2016-02-13 11:45:15 +0900

    jpl ch2 ex02

---
+++ jpl/src/ch02/ex01/Vehicle.java
@@ -0,0 +1,7 @@
+package ch02.ex01;
+
+public class Vehicle {
+	private double speed; // km/h
+	private double angle;
+	private String owner;
+}
---
+++ jpl/src/ch02/ex02/LinkedList.java
@@ -0,0 +1,6 @@
+package ch02.ex02;
+
+public class LinkedList {
+	private Object member;
+	private LinkedList next;
+}
