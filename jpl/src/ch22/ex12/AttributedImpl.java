140efe18b572bda8d721e9a0275517fc8072491a
sakemi <satoshi.sakemi@gmail.com>
2016-08-25 22:45:59 +0900

    ch22ex12

---
+++ jpl/src/ch22/ex12/AttributedImpl.java
@@ -0,0 +1,38 @@
+package ch22.ex12;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import ch22.ex04.Attr;
+import ch22.ex04.Attributed;
+
+public class AttributedImpl implements Attributed, Iterable<Attr> {
+	protected Map<String, Attr> attrTable = new HashMap<String, Attr>();
+
+	@Override
+	public void add(Attr newAttr) {
+		attrTable.put(newAttr.getName(), newAttr);
+	}
+
+	@Override
+	public Attr find(String attrName) {
+		return attrTable.get(attrName);
+	}
+
+	@Override
+	public Attr remove(String attrName) {
+		return attrTable.remove(attrName);
+	}
+
+	@Override
+	public Iterator<Attr> attrs() {
+		return attrTable.values().iterator();
+	}
+
+	@Override
+	public Iterator<Attr> iterator() {
+		return attrs();
+	}
+
+}
---
+++ jpl/src/ch22/ex12/ReadAttrs.java
@@ -0,0 +1,23 @@
+package ch22.ex12;
+
+import java.io.Reader;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+import ch22.ex04.Attr;
+import ch22.ex04.Attributed;
+
+public class ReadAttrs {
+	public static Attributed readAttrs(Reader source) {
+		Scanner in = new Scanner(source);
+		AttributedImpl attrs = new AttributedImpl();
+		Pattern attrPat = Pattern.compile("(.*?)=(.*)");
+		while (in.hasNext()) {
+			in.findInLine(attrPat);
+			MatchResult m = in.match();
+			attrs.add(new Attr(m.group(1), m.group(2)));
+		}
+		return attrs;
+	}
+}
